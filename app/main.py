63b3501f2425f8b64f128b94a0a4c633861bf90f
Chornoknysh <danuk123boss@gmail.com>
2025-08-09 16:33:24 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from typing import Callable, Any, Dict
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    store: Dict[tuple, Any] = {}
+
+    @wraps(func)
+    def wrapper(*args: Any) -> Any:
+        if args in store:
+            print("Getting from cache")
+            return store[args]
+        print("Calculating new result")
+        result = func(*args)
+        store[args] = result
+        return result
+
+    return wrapper
